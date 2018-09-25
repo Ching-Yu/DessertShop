8c84449c19a5a64cd77fa2824e61f7c1bbf1c0c0
kyan28 <zxcv4695152526@gmail.com>
2018-09-25 23:11:07 +0800

    9/25
    9/25

---
+++ src/views/Home.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="home">
+      <nav class="navbar navbar-expand-lg bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+        </button>
+        <a class="navbar-brand navbar-brand-first" href="#"><img src="../assets/images/logo&img/logo-dark.png" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="../assets/images/logo&img/logotype-sm-dark@2x.png" alt=""></a>
+        <a class="navbar-cart" href="#"><i class="fas fa-shopping-cart"></i></a>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">首頁</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">甜點</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">登入</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <img src="../assets/images/annie-spratt-133872-unsplash.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="container category-container">
+        <div class="row">
+          <div class="col-sm-4 category-box">
+            <div class="d-flex align-items-center category">
+              本日精選
+            </div>
+          </div>
+          <div class="col-sm-4 category-box">
+            <div class="d-flex align-items-center category">
+              人氣推薦
+            </div>
+          </div>
+          <div class="col-sm-4 category-box">
+            <div class="d-flex align-items-center category">
+              新品上市
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid content">
+        <div class="row">
+          <div class="content-background"></div>
+          <div class="col contnet-right">
+            <img src="../assets/images/logo&img/lg-為什麼選擇了做甜點@2x.png" alt="" class="">
+            <div class="text-wrapper">
+              <p>
+              青山依舊在，幾度夕陽紅。慣看秋月春風。
+              一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
+              </p>
+              <p>
+                是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid content second-content">
+        <div class="row">
+          <div class="second-conetent-background"></div>
+          <div class="col contnet-right">
+            <img src="../assets/images/logo&img/lg-為什麼選擇了做甜點@2x.png" alt="" class="second-content-img">
+            <div class="text-wrapper">
+              <p>
+              青山依舊在，幾度夕陽紅。慣看秋月春風。
+              一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
+              </p>
+              <p>
+                是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="img-only d-flex justify-content-center">
+        <img src="../assets/images/logo&img/lg-想吃甜點是不需要理由的.png" alt="">
+      </div>
+
+
+      <div class="cart-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="col-md-12">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-img-top">
+                      <span>本日精選</span>
+                      <i class="far fa-heart"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">焦糖馬卡龍<span class="font-weight-bold">$ 450</span></h5>
+                      <a href="#" class="btn">加入購物車</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="col-md-12">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-img-top card-img-top-second">
+                      <span>本日精選</span>
+                      <i class="far fa-heart"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">焦糖馬卡龍<span class="font-weight-bold">$ 450</span></h5>
+                      <a href="#" class="btn">加入購物車</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="col-md-12">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-img-top card-img-top-third">
+                      <span>本日精選</span>
+                      <i class="far fa-heart"></i>
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">焦糖馬卡龍<span class="font-weight-bold">$ 450</span></h5>
+                      <a href="#" class="btn">加入購物車</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="subscribe">
+        <div class="container">
+          <div class="row">
+          <div class="col-lg-6 d-flex align-items-center">
+            <img src="https://public-v2links.adobecc.com/934efdb7-a7e4-47d5-572e-efece0914f62/component?params=component_id:3bcc7bb9-0c8b-4b76-b45c-f320ad20a3bc;version:0&token=1537926969_da39a3ee_2183c34eafc6a9e26f766d35ddea21b5d4b5a15c&api_key=CometServer1" alt="">
+            <span>訂閱你我的甜蜜郵件</span>
+          </div>
+          <div class="col-lg-6 d-flex align-items-center">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text first"><i class="fas fa-envelope"></i></span>
+              </div>
+              <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+              <div class="input-group-append">
+                <button class="btn">
+                  <i class="fas fa-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="contact-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <img src="../assets/images/logo&img/logotype.png" alt="">
+
+              <p class="contact">
+                07-1234-5678<br/>
+                sweetaste@email.com<br/>
+                800 高雄市新興區幸福路 520 號
+              </p>
+            </div>
+            <div class="col-lg-6 text-right">
+              <img src="../assets/images/logo&img/sm-今天是個吃甜點的好日子.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="contact-icon">
+                <a href="#"><i class="fas fa-at"></i></a>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+              </div>
+            </div>
+            <div class="col-lg-6 footer-right">
+              <p>© 2018 Sweetaste* All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/Home.scss';
+</style>
+
